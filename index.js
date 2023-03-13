0f375f2dd5aed005a2e859c151b193fbd50cc7af
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-03-12 22:27:57 -0400

    Adss myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {key:{}}; {
+    value:{}
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return newEmployee (
+    const newEmployee = {...employee};
+    newEmployee[key] = value);
+    
+    
+ } 
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+        employee.streetAddress = "12 Broadway";
+        return employee;
+    }
+    function deleteFromEmployeeBykey(employee, key){
+        const newEmployee = "Sam";
+        return newEmployee
+        const newSreetAddress ='11 Broadway';
+        return newStreetAddress;
+    }
+    function destructivelyDeleteFromEmployeeByKey(employee,key) {
+        delete employee[key];
+        return employee;
+    }
+    
